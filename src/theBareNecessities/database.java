e99a9c2bdecb4de9e21bb5a135653d5826c8e8aa
helemork <helemork@gmail.com>
2017-03-12 17:42:27 +0100

    nå kan man legge til øvelser. Man kan ikke gjøre noe med dem utover å legge dem til. Keep it simple lissom.

--- src/theBareNecessities/database.java
+++ src/theBareNecessities/database.java
@@ -16,17 +16,116 @@ public class database {
     private static String mysqlUser = "theaks_test";
     private static String mysqlPass = "TKStestDB";
 
-
-
-    public static String test(){
-        try {
+    // HJELPEFUNKSJON FOR Å KOBLE OSS TIL DB SÅ VI IKKE MÅ DET MASSE
+    public static Connection connectToDb(){
+    	try {
             Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        } 
+    	catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    	Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(mysqlAddr, mysqlUser, mysqlPass);
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	return conn;
+    };
+    
+    public static String createOppvarming(String name, String description, int duration){
+    	// LEGG INN EN OPPVARMING ELLER AVLUTNINGSØVELSE (alle uten resultat mao)
+    	
+    	Connection conn = connectToDb();
+    	try{
+    		PreparedStatement stmt = conn.prepareStatement("INSERT INTO Ovelse (ovelsesnavn,beskrivelse) VALUES (?,?)");
+    		stmt.setString(1, name);
+    		stmt.setString(2, description);
+    		stmt.executeUpdate();
+    		stmt = conn.prepareStatement("INSERT INTO OppvarmingAvslutning (ovelsesnavn,varighet) VALUES (?,?)");
+    		stmt.setString(1, name);
+    		stmt.setInt(2, duration);
+    		stmt.executeUpdate();
+    	}
+    	catch (SQLException e){
+    		return e.toString();
+    	};
+    	
+    	return "OK";
+    };
+    
+    public static String createAnnet(String name, String description, String notat){
+    	// LEGG INN EN annen type øvelse (alle uten resultat mao)
+    	
+    	Connection conn = connectToDb();
+    	try{
+    		PreparedStatement stmt = conn.prepareStatement("INSERT INTO Ovelse (ovelsesnavn,beskrivelse) VALUES (?,?)");
+    		stmt.setString(1, name);
+    		stmt.setString(2, description);
+    		stmt.executeUpdate();
+    		stmt = conn.prepareStatement("INSERT INTO Annet (ovelsesnavn,notat) VALUES (?,?)");
+    		stmt.setString(1, name);
+    		stmt.setString(2, notat);
+    		stmt.executeUpdate();
+    	}
+    	catch (SQLException e){
+    		return e.toString();
+    	};
+    	
+    	return "OK";
+    };
+    
+    public static String createKondisjonStyrke(String name, String description, int belastning, int Antallrepitisoner, int Antallsett  ){
+    	// LEGG INN EN annen type øvelse (alle uten resultat mao)
+    	
+    	Connection conn = connectToDb();
+    	try{
+    		PreparedStatement stmt = conn.prepareStatement("INSERT INTO Ovelse (ovelsesnavn,beskrivelse) VALUES (?,?)");
+    		stmt.setString(1, name);
+    		stmt.setString(2, description);
+    		stmt.executeUpdate();
+    		stmt = conn.prepareStatement("INSERT INTO KondisjonStyrke (ovelsesnavn,Belastning,Antallrepitisoner,Antallsett) VALUES (?,?,?,?)");
+    		stmt.setString(1, name);
+    		stmt.setInt(2, belastning);
+    		stmt.setInt(3, Antallrepitisoner);
+    		stmt.setInt(4, Antallsett);
+    		stmt.executeUpdate();
+    	}
+    	catch (SQLException e){
+    		return e.toString();
+    	};
+    	
+    	return "OK";
+    };
+    
+    public static String createUtholdenhet(String name, String description, float lengde){
+    	// LEGG INN EN annen type øvelse (alle uten resultat mao)
+    	
+    	Connection conn = connectToDb();
+    	try{
+    		PreparedStatement stmt = conn.prepareStatement("INSERT INTO Ovelse (ovelsesnavn,beskrivelse) VALUES (?,?)");
+    		stmt.setString(1, name);
+    		stmt.setString(2, description);
+    		stmt.executeUpdate();
+    		stmt = conn.prepareStatement("INSERT INTO Utholdenhet (ovelsesnavn,Lengde) VALUES (?,?)");
+    		stmt.setString(1, name);
+    		stmt.setFloat(2, lengde);
+    		stmt.executeUpdate();
+    	}
+    	catch (SQLException e){
+    		return e.toString();
+    	};
+    	
+    	return "OK";
+    };
+    
+
+    public static String test(){
+        // PRINTER UT DET SISTE SOM ER LAGT TIL. SÅ VI VET AT DET VIRKER SJØ.
         
         try{
-            Connection conn = DriverManager.getConnection(mysqlAddr, mysqlUser, mysqlPass);
+            Connection conn = connectToDb();
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Ovelse");
 
             ResultSet rs = stmt.executeQuery();
--- src/theBareNecessities/main.java
+++ src/theBareNecessities/main.java
@@ -5,6 +5,27 @@ import theBareNecessities.database;
 
 class Main {
     public static void main(String[] args) {
+    	/*System.out.println(database.createOppvarming("biljard", 
+    			"lat som du er en biljardkule og spring rundt. Krever medspillere", 
+    			53));
+    	
+    	System.out.println(database.createAnnet("portrening slalom", 
+    			"øv på å kjøre mellom porter", 
+    			"repeter 5 ganger")); 
+    	
+    	
+    	System.out.println(database.createKondisjonStyrke("pushØps", "dytt deg opp fra bakken", 0, 12, 4));
+    	
+    	*/
+    	
+    	
+    	System.out.println(database.createUtholdenhet("springe", "en fot etter den andre", 34.23f));
+    	
+    	
+    	
+    	
+    	
+    	
     	System.out.println(database.test()); // Display the string.
     	
     	///adding a branch stuff

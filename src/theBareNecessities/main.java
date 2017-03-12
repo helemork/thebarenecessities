bef817bfe85f8203dacb73cefc65c24088f5cc7d
helemork <helemork@gmail.com>
2017-03-12 21:16:55 +0100

    usercase 2 og 5 complete. Helene out.

--- src/theBareNecessities/main.java
+++ src/theBareNecessities/main.java
@@ -1,34 +1,85 @@
 package theBareNecessities;
 
+import java.util.Scanner;
+
 import theBareNecessities.database;
 
 
 class Main {
     public static void main(String[] args) {
-    	/*System.out.println(database.createOppvarming("biljard", 
-    			"lat som du er en biljardkule og spring rundt. Krever medspillere", 
-    			53));
     	
-    	System.out.println(database.createAnnet("portrening slalom", 
-    			"øv på å kjøre mellom porter", 
-    			"repeter 5 ganger")); 
+    	Scanner scanner = new Scanner(System.in);
+    	while (true){
+    		System.out.println("Ka vil du gjør no?");
+    		System.out.println("1) vis aillj øvelsain din");
+    		System.out.println("2) lægg te ny øvelse");
+    		System.out.println("3) nåkka ainnja. Avslutt.");
+    		System.out.println("Vælg en av tallan over:");
+    		int choice = scanner.nextInt();
+    		if (choice == 1){
+    			System.out.println(database.getAllOvelse()); 
+    		}
+    		else if (choice == 2){
+    			System.out.println("Kossn type øvelse ska det værra?");
+        		System.out.println("1) oppvarming eller avslutningsøvels");
+        		System.out.println("2) utholdenhetsøvels");
+        		System.out.println("3) styrkeøvels eller kondisjonsøvels");
+        		System.out.println("4) nå ainnja");
+        		System.out.println("Vælg en av tallan over:");
+        		choice = scanner.nextInt();
+        		
+        		System.out.println("skriv inn navn på øvelse:");
+        		scanner.nextLine();
+        		String name = scanner.nextLine();
+        		
+        		System.out.println("skriv inn beskrivelse av øvelsa:");
+        		String description = scanner.nextLine();
+        		
+        		if (choice == 1){
+        			System.out.println("skriv innj kor mang minutt det ska var:");
+            		int duration = scanner.nextInt();
+        			System.out.println(database.createOppvarming(name, 
+        			description, 
+        			duration));
+        		}
+        		
+        		else if (choice == 2){
+        			System.out.println("skriv inn kor langt det ska vær:");
+            		float lengde = scanner.nextFloat();
+        			System.out.println(database.createUtholdenhet(name, description, lengde));
+        		}
+        		
+        		
+        		else if (choice == 3){
+        			System.out.println("Kor myttji belastning (heiltallj):");
+        			int belastning = scanner.nextInt();
+        			System.out.println("Kor mang repitisjona (heiltallj):");
+        			int Antallrepitisoner = scanner.nextInt();
+        			System.out.println("Kor mang sett (heiltallj):");
+        			int Antallsett = scanner.nextInt();
+        			System.out.println(database.createKondisjonStyrke(name, description, belastning, Antallrepitisoner, Antallsett));
+        		}
+        		
+
+        		
+        		else if (choice == 4){
+        			System.out.println("skriv inn notat om du har nånn:");
+            		String notat = scanner.nextLine();
+            		System.out.println(database.createAnnet(name, description, notat));
+        		}
+    			
+    			
+    			
+    		}
+    		else if (choice == 3){
+    			System.out.println("No skrur æ mæ av");
+    			break;
+    		};
+    		
+    	}
+    	scanner.close();
     	
-    	
-    	System.out.println(database.createKondisjonStyrke("pushØps", "dytt deg opp fra bakken", 0, 12, 4));
-    	
-    	
-    	
-    	
-    	System.out.println(database.createUtholdenhet("springe", "en fot etter den andre", 34.23f));
-    	
-    	*/
-    	
-    	
-    	
-    	
-    	System.out.println(database.getAllOvelse()); // Display the string.
-    	
-    	///adding a branch stuff
+ 
     	
     }
 }
